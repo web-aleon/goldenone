3f1f297a99a7509256b0c0ba557b204fd1ba4293
Alexey Leontiev <web-a.leon@ukr.net>
2017-03-08 00:30:15 +0200

    Responsive Header Height AndAbout Us section

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*ADAPTIV HEIGHT HOME SECTION*/
+function wbh_adaptive_section(){
+	let section = document.getElementById('home');
+	let elemWidth = section.clientWidth;
+	let elemHeight = elemWidth / 2;
+
+	section.style.height = elemHeight + 'px';
+}
+wbh_adaptive_section();
+
+window.onresize = wbh_adaptive_section;
